013d645385f8da01973e1eddbf24228e133f4a30
Viktor Pozhydaiev <jude.peper@gmail.com>
2021-07-02 22:50:13 +0300

    добавляет решение 2 и 3 задач

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,7 +6,13 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
-const listItem = document.createElement('li');
-console.log(listItem);
-ingredients.appendChild(#ingredients)
-console.log(ingredients)
+const listContainer = document.querySelector(`ul#ingredients`);
+// console.log(listContainer)
+const insertListItems = items => {
+  const unit = document.createElement(`li`);
+  unit.textContent = items;
+  return unit;
+};
+  
+const allUnits = ingredients.map(items => insertListItems(items));
+listContainer.append(...allUnits);
