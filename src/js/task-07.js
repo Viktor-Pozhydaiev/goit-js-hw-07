17719bcc8a86986475f7b6b4b150dace9204dcde
Viktor Pozhydaiev <jude.peper@gmail.com>
2021-07-03 22:51:28 +0300

    добавляет решение 7 задания

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,3 +1,10 @@
-function onInput(event) {
-   textEl.style.fontSize = event.currentTarget.value + "px";
- }
+const inputRange = document.querySelector('#font-size-control');
+const textSpan = document.querySelector('#text');
+
+inputRange.addEventListener('input', onInputRange);
+
+function onInputRange(event) {
+  let startValueRange = +event.target.value;
+
+  textSpan.style.fontSize = `${startValueRange + 0.1}px`;
+}
